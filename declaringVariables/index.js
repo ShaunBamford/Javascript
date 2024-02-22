ec027c9ca5a87e76a1a37e4b5a64d2d97b344f13
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-02-22 03:18:35 +0000

    Create index.js

---
+++ declaringVariables/index.js
@@ -0,0 +1,24 @@
+// Variables
+
+// Declaring variable
+let x;
+// Assigning a variable
+x = 100;
+// Declaring and assigning
+let y = 123;
+
+// Using specfic mathematical operators in a variable
+x = x ** 3 // To the power of
+let extraX = x % 3; // Modulus
+
+
+let email = "Test@test.com"
+// Replacing the <h1> and <p> on the frontend
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = `Your email is ${email}`;
+
+
+// Tell the console what datatype a variable is
+console.log(typeof email);
+// Tell the console the email
+console.log(`Your email is ${email}`);
